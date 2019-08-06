d1f22a18c2dfc3c6257855fe08834510dd8d2ed0
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2019-08-06 19:15:43 +0530

    Create finding words.cpp

---
+++ finding words.cpp
@@ -0,0 +1,12 @@
+void recurse(char last_letter,string last_word,TrieNode *node)
+{
+    TrieNode *current=node;
+    auto it=(current->children).begin();
+    if(current->endofword==true);
+        cout<<last_word+last_letter<<endl;
+    while(it!=(current->children).end())
+    {
+        recurse(it->first,last_word+last_letter,it->second);
+        it++;
+    }
+}
