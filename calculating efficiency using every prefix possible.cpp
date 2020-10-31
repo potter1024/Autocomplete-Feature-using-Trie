69c64bdfca43964b190bc69a1075718a83d0c114
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2020-10-31 13:54:45 +0530

    Update calculating efficiency using every prefix possible.cpp

--- calculating efficiency using every prefix possible.cpp
+++ calculating efficiency using every prefix possible.cpp
@@ -1,22 +1,17 @@
     vector<ld> re;
-    for(ll h=0;h<20000;h++)
-    {
-        for(ll hh=1;hh<v[h].size();hh++)
-        {
+    for(ll h=0;h<20000;h++){
+        for(ll hh=1;hh<v[h].size();hh++){
             clock_t startTime=clock();
             string s{v[h].begin(),v[h].begin()+hh};
             suggestions(s);
             ld x1=(double(clock()-startTime )/(double)CLOCKS_PER_SEC);
             startTime=clock();
-            for(ll i=0;i<20000;i++)
-            {
+            for(ll i=0;i<20000;i++){
                 if(v[i].size()<s.size())
                     continue;
                 ll count=0;
-                for(ll j=0;j<s.size();j++)
-                {
-                    if(v[i][j]==s[j])
-                    {
+                for(ll j=0;j<s.size();j++){
+                    if(v[i][j]==s[j]){
                         count++;
                     }
                 }
@@ -25,8 +20,7 @@
             ld x2=(double(clock()-startTime )/(double)CLOCKS_PER_SEC);
             re.pb(x2/x1);
         }
-        if(h%1000==0)
-        {
+        if(h%1000==0){
             cout<<((ld)h/((ld)(20000)))*100<<"% completed"<<endl;
         }
     }
